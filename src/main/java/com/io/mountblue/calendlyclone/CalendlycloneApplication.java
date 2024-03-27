198cec5001a19129f6d611128c699d53c8de322b
VishwjeetKumar <vk7783.mk@gmail.com>
2024-03-27 15:36:18 +0530

    Initial commit

---
+++ src/main/java/com/io/mountblue/calendlyclone/CalendlycloneApplication.java
@@ -0,0 +1,13 @@
+package com.io.mountblue.calendlyclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendlycloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendlycloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/mountblue/calendlyclone/CalendlycloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.mountblue.calendlyclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendlycloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
