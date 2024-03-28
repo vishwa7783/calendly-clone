d62f4af0f472d582ff4ddb36abeebcbc7c9bdaeb
BUDDU  V R S SRI HARSHA <buddu.harsha.27.2@mountblue.tech>
2024-03-28 10:27:32 +0530

    Added user controller,user repository and user service

---
+++ src/main/java/com/io/mountblue/calendlyclone/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.io.mountblue.calendlyclone.Repository;
+
+import com.io.mountblue.calendlyclone.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+
+    User findUserByEmail(String email);
+}
